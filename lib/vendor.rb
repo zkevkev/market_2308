9e414094d5fdcd2c245ccf352ca68d83d1aab3db
Kevin Zolman <kevin.zolman@gmail.com>
2023-09-18 09:26:58 -0600

    Functionalize vendor class

---
+++ lib/vendor.rb
@@ -0,0 +1,16 @@
+class Vendor
+  attr_reader :name, :inventory
+
+  def initialize(name)
+    @name = name
+    @inventory = Hash.new(0)
+  end
+
+  def check_stock(item)
+    @inventory[item]
+  end
+
+  def stock(item, quantity)
+    @inventory[item] += quantity
+  end
+end
