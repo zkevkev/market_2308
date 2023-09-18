ec9945242adb5240d743ee03dfea824ae58018c3
Kevin Zolman <kevin.zolman@gmail.com>
2023-09-18 09:09:20 -0600

    Add item class and associated tests

---
+++ spec/item_spec.rb
@@ -0,0 +1,22 @@
+require 'rspec'
+require './lib/item'
+# require './lib/vendor'
+
+Rspec.describe Item do
+  before(:each) do
+    @item1 = Item.new({name: 'Peach', price: "$0.75"})
+    @item2 = Item.new({name: 'Tomato', price: '$0.50'})
+    # @vendor = Vendor.new("Rocky Mountain Fresh")
+  end
+
+  describe '#initialize' do
+    it 'exists' do
+      expect(@item1).to be_an_instance_of(Item)
+    end
+
+    it 'has attributes' do
+      expect(@item2.name).to eq("Tomato")
+      expect(@item2.price).to eq(0.5)
+    end
+  end
+end
